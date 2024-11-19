417e939d7cc1aad75ea047018609aa14752d6b97
Sarp <240201456@ostimteknik.edu.tr>
2024-11-19 18:27:42 +0300

    Initial Commit!

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+
+    const [url, setUrl] = useState("")
+    const [shortenedUrl, setShortenedUrl] = useState("")
+
+    const shorten = async () => {
+        const response = await fetch('/short', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({url})
+        })
+        const data = await response.json()
+        setShortenedUrl(`${window.location.href}${data.shortUrl}`)
+    }
+
+    return (
+        <>
+            <div id={"App"}>
+                <span id={"Title"}>SİKTİRT.ME</span>
+                <span id={"SubTitle"}>Hızlı, Güvenli, Kısa.</span>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    shorten()
+                }}>
+                    <input id={"URLInput"} type={"url"} value={url} onChange={(e) => {
+                        setUrl(e.target.value)
+                    }} placeholder={"https://sikerler.online"}
+                    />
+                    <button id={"ShortenButton"} type={"submit"}>Kısalt</button>
+                </form>
+            </div>
+            <span id={"ShortenedURLCopy"} onTouchStart={() =>{navigator.clipboard.writeText(shortenedUrl)}} onClick={() =>{navigator.clipboard.writeText(shortenedUrl)}}>{shortenedUrl}</span>
+        </>
+    )
+}
+
+export default App
